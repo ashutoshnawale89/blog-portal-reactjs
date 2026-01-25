8cdbab3da16a35dec90eefc4a1164a4f96c3fe1a
ASHUTOSH\ashut <ashutosh.r@maatrum.com>
2026-01-26 01:08:28 +0530

    Logo change

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { BlogProvider } from './context/BlogContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BlogProvider>
-      <BrowserRouter basename="/blog-portal-reactjs">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </BlogProvider>
   </React.StrictMode>
 )
+
